6294e5589652c239b0a32d8424e1f21f31dffbe3
Skruphold <hunterco999@gmail.com>
2022-02-22 14:56:24 -0600

    added more to webpack and got logo image working also got the put and get routes done.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,6 +24,24 @@ module.exports = () => {
         title: 'Just Another Text Editor'
       }),
       new MiniCssExtractPlugin(),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor for all your text editing needs!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
